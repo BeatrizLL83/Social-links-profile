bc8c9bbebec522fac74e651089e8978883d042e4
BeatrizLL83 <beatrizlopezleon83@gmail.com>
2026-02-22 22:11:46 +0100

    Second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.social-btn'); // "Tráeme todos los botones sociales"
+
+const botones = document.querySelectorAll('.social-btn');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        // Esto solo si quieres que pase algo ANTES de ir al link
+        console.log("Navegando a: " + boton.textContent);
+    });
+});
