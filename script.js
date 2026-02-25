2256b52b88445c462f129f066f18016d6b46421f
BeatrizLL83 <beatrizlopezleon83@gmail.com>
2026-02-25 10:16:01 +0100

    fourth commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelectorAll('.social-btn'); // "Tráeme todos los botones sociales"
+document.querySelectorAll(".social-btn"); // "Tráeme todos los botones sociales"
 
-const botones = document.querySelectorAll('.social-btn');
+const botones = document.querySelectorAll(".social-btn");
 
-botones.forEach(boton => {
-    boton.addEventListener('click', () => {
+botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
         // Esto solo si quieres que pase algo ANTES de ir al link
         console.log("Navegando a: " + boton.textContent);
     });
